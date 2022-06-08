accb98015afa8dc478b8f2e5baa22bb532979e3b
Hastieiliat <hasti.eiliat@kautex.textron.com>
2022-06-08 18:00:02 -0400

    module15_challenge2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,4 +4,7 @@ lm(vehicle_length ~ mpg,MechaCar_mpg) #create linear model
 lm(vehicle_weight ~ mpg,MechaCar_mpg) 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg))
-        
+  
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)      
+total_summary <- Suspension_Coil %>% group_by() %>% summarize(Mean=mean(PSI),median=median(PSI),Variance=var(PSI),SD= sd(PSI), .groups = 'keep') #create summary table with multiple columns
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),median=median(PSI),Variance=var(PSI),SD= sd(PSI), .groups = 'keep') #create summary table with multiple columns
