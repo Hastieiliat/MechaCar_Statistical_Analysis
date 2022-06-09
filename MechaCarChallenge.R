5a02a3d8c0e4462cbd22eaad16d4e5d88ef4619f
Hastieiliat <hasti.eiliat@kautex.textron.com>
2022-06-09 00:03:47 -0400

    module 15

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,9 +8,7 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)      
 total_summary <- Suspension_Coil %>% group_by() %>% summarize(Mean=mean(PSI),median=median(PSI),Variance=var(PSI),SD= sd(PSI), .groups = 'keep') #create summary table with multiple columns
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),median=median(PSI),Variance=var(PSI),SD= sd(PSI), .groups = 'keep') #create summary table with multiple columns
-t.test(log10(Suspension_Coil$PSI),mu=1500) #compare sample versus population means
-?t.test()
-
+t.test(log10(Suspension_Coil$PSI),mu=log10(1500)) #compare sample versus population means
 Lot_1 <- Suspension_Coil %>% filter(Manufacturing_Lot=="Lot1") #select only data points where Lot1
 Lot_2 <- Suspension_Coil %>% filter(Manufacturing_Lot=="Lot2") #select only data points where Lot2
 Lot_3 <- Suspension_Coil %>% filter(Manufacturing_Lot=="Lot3") #select only data points where Lot3
